72d0776254f83301920c4db845865253ff446222
Ponick07 <luanponick.sz.2@gmail.com>
2023-10-07 16:03:29 -0300

    004

--- UberPets/main.c
+++ UberPets/main.c
@@ -4,10 +4,16 @@
 #include <regex.h>
 #include <unistd.h>
 
-int main(void){
-    char userName[] = "";    
-    //end declarentions
+void pedirNome( char** name[] );
+void pedirConfirmacao( char** resposta[], bool regex, char value[], char nomeFuncao[] );
+void tipoDeCadastro( char **valor[] );
 
+int main(){
+    char userName[] = ""; 
+    char escolhaCadastroDeUsuario[] = "";
+    //end declarentions
+    
+    //tipoDeCadastro( escolhaCadastroDeUsuario );
     pedirNome( userName );
     printf(userName);
     
@@ -16,43 +22,69 @@ int main(void){
 
 void pedirNome(char** name[]){
     regex_t regex;
-    int reti;
+   
     
     pedirNomeInicio:
     //pega nome de entrada
     printf( "Digite o nome do usuario: " );
-    fgets( name, sizeof(name), stdin ); 
+    fgets( name, sizeof(name), stdin );
+    char resp[] = ""; 
+    pedirConfirmacao(resp, true, name, "pedirNome");
+    printf("%s: animal",resp);
     
+}
+void pedirConfirmacao( char** resposta[], bool regex, char value[], char nomeFuncao[]){
+    //regra regex -> nao permitir numero! 
+    int reti;
     reti = regcomp( &regex, "^[^0-9]*$", 0 );
-    reti = regexec( &regex, name, 0, NULL, 0 );
+    reti = regexec( &regex, value, 0, NULL, 0 );
     
     pedirNomeConfirmacao:
-
-    //testa se o nome é valido
-    if ( reti ) {
-        printf( "favor digite um nome valido...\n " );
-        usleep( 2000000 );
-        goto pedirNomeInicio;
-    }
-    
+         
+    //testa se o nome é valido na regra do regex
+    if ( reti && regex) {
+        printf("antes");
+        //printf( "favor não digite numeros...\n " );
+        //usleep( 2000000 );
+        strcat(nomeFuncao, "Inicio");
+        resposta = nomeFuncao;
+        printf("teste %s",resposta);
+    }   
     //confirmaçao
     char chose[] = ""; 
-    printf( "voce digitou %s", name );
+    printf( "voce digitou %s", value );
     printf( "esta correto? (Y/N)" );
     scanf(" %c", chose);
-    
-    //transforma toda a entrada em maisculas.
-    for (int i = 0; chose[i] != '\0'; i++) {
-        chose[i] = toupper(chose[i]);
-    }
+    chose[0] = toupper(chose[0]);
     
     //testa se o resultado de entrada é Y/y caso nao ele pergunta novamente
     if( strcmp( chose, "Y") == 0 ){
+        resposta = "ok";
         return;
     }else if( strcmp( chose, "N") == 0 ){
-        goto pedirNomeInicio;
+        resposta = "pedirNomeInicio";
+    }else{
+        printf("favor digite Y ( sim ) ou N ( não )... \n");
+        usleep(1000000);
+        goto pedirNomeConfirmacao;
     }
-    printf("favor digite Y ( sim ) ou N ( não )... \n");
+}
+
+void tipoDeCadastro( char **valor[] ){
+    char temp;
+    
+    tipoDeCadastroInicio:
+    printf( "qual seria a forma de cadastro? " );
+    printf( "( 1 ) Cliente " );
+    printf( "( 2 ) Motorista " );
+    fgets( temp, sizeof(temp), stdin ); 
+    if(strcmp( temp, "1") == 0 ){
+        return;
+    }else if( strcmp( temp, "2") == 0 ){
+        return;
+    }
+    printf("favor digite Um valor valido... \n");
     usleep(1000000);
-    goto pedirNomeConfirmacao;
+    goto tipoDeCadastroInicio;
+    
 }
--- teste.c
+++ teste.c
@@ -2,16 +2,26 @@
 #include <stdbool.h>
 #include <string.h>
 
+void testeFuncao(char teste1[], bool teste2);
 int main(){
-    char minhaString[] = "Hello, World!";
-    int i;
+    // char minhaString[] = "Hello, World!";
+    // int i;
 
-    for (i = 0; minhaString[i] != '\0'; i++) {
-        minhaString[i] = toupper(minhaString[i]);
-    }
+    // for (i = 0; minhaString[i] != '\0'; i++) {
+    //     minhaString[i] = toupper(minhaString[i]);
+    // }
 
-    printf("String em maiúsculas: %s\n", minhaString);
+    // printf("String em maiúsculas: %s\n", minhaString);
 
-    return 0;
+    // return 0;
+
+    char teste[] = "valor foda";
+    bool top = true;
+
+    testeFuncao(teste, top);
+}
+
+void testeFuncao( char teste1[], bool teste2){
+    printf("%s %d", teste1, teste2);
 }
 
